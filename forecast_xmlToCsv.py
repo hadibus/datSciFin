2e6ab7595200fabd428d00f3e57a54996e1de753
Christopher <chris.jenkins20102012@gmail.com>
2017-11-28 19:40:31 -0700

    finished forecast csv and code

--- forecast_xmlToCsv.py
+++ forecast_xmlToCsv.py
@@ -13,81 +13,36 @@ from dateutil.parser import parse
 
 # And this will be used to process date objects.
 import datetime
+import time
 
-root = ET.parse(sys.argv[1])
+sys.stdout.write("Time,fd1,fd2,fd3,fd4,fd5,fd6,fd7,fn1,fn2,fn3,fn4,fn5,fn6,fn7\n") 
 
+for idx in range(len(sys.argv)):
+	if idx == 0:
+		continue
+	try:
+		tree = ET.parse(sys.argv[idx])
+		root = tree.getroot()
+	except IOError:
+		print("no parse on " + sys.argv[idx])
+		continue
 
-# Extract the time layouts.
-# We will store them in a dictionary for easy retrieval.
-timelayouts = {}
-# Use xpath to find the time-layout elements.
-for timelayoutelement in root.findall("./data/time-layout"):
-	# Get the layout key for each.
-	layoutkey = timelayoutelement.find("layout-key").text
-	# Retrieve and store the start times as a list.
-	starttimes = []
-	for starttimeelement in timelayoutelement.findall("start-valid-time"):
-		starttimes.append(parse(starttimeelement.text))
-	# Retrieve and store the start times as a list.
-	endtimes = []
-	for endtimeelement in timelayoutelement.findall("end-valid-time"):
-		endtimes.append(parse(endtimeelement.text))
-	# Store in the timelayouts dictionary.
-	timelayouts[layoutkey] = [starttimes, endtimes]
+	#start row with date
+	dtTime = datetime.datetime.strptime(sys.argv[idx], "webXml%Y-%m-%d %H:%M:%S.xml")
+	sys.stdout.write(dtTime.strftime("%Y%m%d%H%M"))
 
-highTimes = timelayouts['k-p24h-n7-2'][0]
-lowTimes = timelayouts['k-p24h-n7-1'][0]
-	
-# Extract the weather parameters
-# We will store them in a dictionary for easy retrieval.
-weatherparameters = {}
-for parameterelement in root.find("./data/parameters/."):
-	# The tag name is a short name for the type of weather element.
-	parameterelementtag = parameterelement.tag
-	# The attributes are returned as a dictionary.
-	parameterelementattribs = parameterelement.attrib
-	# This name is better for use than the tag name since it is unique.
-	parameterelementname = parameterelement.find("name").text
-	if parameterelementtag == "temperature":
-		# Retrieve and store the values as a list.
-		parametervalues = []
-		for value in parameterelement.findall("value"):
-			parametervalues.append(value.text)
-		# Store in the weatherparameters dictionary.
-		weatherparameters[parameterelementname] = {"tag" : parameterelementtag , "attribs" : parameterelementattribs , "values" : parametervalues}
-	elif parameterelementtag == "conditions-icon":
-		# Retrieve and store the values as a list.
-		parametericonlinks = []
-		for iconlink in parameterelement.findall("icon-link"):
-			parametericonlinks.append(iconlink.text)
-		# Store in the weatherparameters dictionary.
-		weatherparameters[parameterelementname] = {"tag" : parameterelementtag , "attribs" : parameterelementattribs , "iconlinks" : parametericonlinks}	
-	elif parameterelementtag == "weather":
-		# Retrieve and store the values as a list.
-		parameterweatherconditions = []
-		for weathercondition in parameterelement.findall("weather-conditions"):
-			weathervalue = weathercondition.find("value")
-			if weathervalue is None:
-				weatherstr = "clear"
-			else:
-				weatherstr = weathervalue.attrib["coverage"] + " " + weathervalue.attrib["intensity"] + " " + weathervalue.attrib["weather-type"]
-			parameterweatherconditions.append(weatherstr)
-		# Store in the weatherparameters dictionary.
-		weatherparameters[parameterelementname] = {"tag" : parameterelementtag , "attribs" : parameterelementattribs , "weatherconditions" : parameterweatherconditions}
-
-#pp.pprint(weatherparameters)
-#print len(weatherparameters)
-
-maxTemps = weatherparameters["Daily Maximum Temperature"]["values"]
-minTemps = weatherparameters["Daily Minimum Temperature"]["values"]
-
-for ltim, mint, htim, maxt in zip(lowTimes, minTemps, highTimes, maxTemps):
-	sys.stdout.write(ltim.strftime("%Y-%m-%d %H:%M:%S %z"))
-	sys.stdout.write(", ")
-	sys.stdout.write(mint)
+	try:
+		temps = root.find("data").find("parameters").findall("temperature")
+	except AttributeError:
+		sys.stdout.write(",,,,,,,,,,,,,,\n")
+		continue
+	for temp in temps:
+		if temp.get('type') == 'hourly':
+			continue
+		vals = temp.findall("value")
+		for idxVals in range(7):
+			sys.stdout.write(",")
+			if idxVals < len(vals):
+				sys.stdout.write(vals[idxVals].text)
 	sys.stdout.write("\n")
 
-	sys.stdout.write(htim.strftime("%Y-%m-%d %H:%M:%S %z"))
-	sys.stdout.write(", ")
-	sys.stdout.write(maxt)
-	sys.stdout.write("\n")
